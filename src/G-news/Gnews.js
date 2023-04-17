e0e755853a785fb159bdd41cf7b8659b5edab755
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-17 21:29:44 +0300

    modified page

--- src/G-news/Gnews.js
+++ src/G-news/Gnews.js
@@ -1,11 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { gameNews } from './gamingnews'
+import './Gnews.css'
 
 function Gnews() {
     const[news,setNews]=useState([])
 
+    useEffect(()=>{
+        gameNews()
+        .then(({data})=>{
+            setNews(data)
+            console.log(data);
+        })
+    },[])
+
+    const Ndata=news?.map((mynews)=>{
+        const{date,description,image,link,title,id}=mynews
+
+        return(
+            <div key={id} className='paper' elevation={2}>
+                <div className='title'>
+                    <div>
+                        {title}
+                    </div>
+                </div>
+                <div className='images'>
+                    <img src={image} alt={title} className='img'/>
+                </div>
+                <div className='description'>
+                    <div>
+                        {description}
+                    </div>
+                </div>
+                <div>
+                    {date}
+                </div>
+                <div>
+                    <a href={link} target='_blank' rel="noreferrer" >Visit Site</a>
+                </div>
+                
+            </div>
+
+        )
+    })
+
+    
+
   return (
-    <div>Gnews</div>
+    <div className='newspage'>
+        {Ndata}
+    </div>
   )
 }
 
