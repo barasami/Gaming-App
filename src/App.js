9dee95acf575ca059b7fda625b342e054a353d5c
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-19 20:00:45 +0300

    modified page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Gnews from './G-news/Gnews';
 import Rnews from './R-leases/Rnews';
 import Homeonsale from './Onsale/Homeonsale';
+import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { Fragment } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Gnews/>
-      <Rnews/>
-      <Homeonsale/>
+      <Router>
+        <Fragment>
+          <Routes>
+            <Route path='' element={<Gnews/>}/>
+            <Route path='' element={<Rnews/>}/>
+            <Route path='' element={<Homeonsale/>}/>
+          </Routes>
+        </Fragment>
+      </Router>
     </div>
   );
 }
