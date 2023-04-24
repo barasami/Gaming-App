398c4270063d2a8b00ffc8ec4dfbf88d79c5876f
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-24 08:16:00 +0300

    modified form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,10 +2,21 @@ import React from 'react'
 import './Form.css'
 
 function Form() {
+
+  const submitMe=(e)=>{
+    e.preventDefault()
+  }
   return (
-    <div>
-      <div>
-        <p>Hello there</p>
+    <div className='coolform'>
+      <div className='formdetail'>
+        <form onSubmit={submitMe} className='myform'>
+          <div className='input'>
+            <input className='myinput' type='text' placeholder='Search Game....'/>
+          </div>
+          <div className='btn'>
+            <button className='mybtn'>Search</button>
+          </div>
+        </form>
       </div>
     </div>
   )
